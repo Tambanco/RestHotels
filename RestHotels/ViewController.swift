79d005eb220ef8ecd2e69cf0a50e84432f86b0a8
Tambanco <tmbnc@bk.ru>
2020-10-24 17:02:35 +0300

    first commit

---
+++ RestHotels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestHotels
+//
+//  Created by Tambanco on 24.10.2020.
+//  Copyright © 2020 Tambanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
