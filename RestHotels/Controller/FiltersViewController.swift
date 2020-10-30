bec48133c2c6db8c90ec4d7f601797fdf2b066d6
Tambanco <tmbnc@bk.ru>
2020-10-31 02:43:06 +0300

    added UserDefault switch case

--- RestHotels/Controller/FiltersViewController.swift
+++ RestHotels/Controller/FiltersViewController.swift
@@ -19,6 +19,8 @@ class FiltersViewController: UIViewController {
     var delegate: CanReceive?
     var sourceHotels = [DataModel]()
     
+    @IBOutlet weak var doneButtonLbl: UIButton!
+    @IBOutlet weak var resetButtonLbl: UIButton!
     @IBOutlet weak var sortDistanceLbl: UILabel!
     @IBOutlet weak var sortRoomsLbl: UILabel!
     @IBOutlet weak var distanceSwitch: UISwitch!
@@ -27,19 +29,35 @@ class FiltersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        distanceSwitch.isOn = UserDefaults.standard.bool(forKey: "distanceSwitchCase")
+        roomsSwitch.isOn = UserDefaults.standard.bool(forKey: "roomsSwitchCase")
+        
         distanceSwitch.backgroundColor = UIColor.lightGray
         distanceSwitch.layer.cornerRadius = 16.0
         roomsSwitch.backgroundColor = UIColor.lightGray
         roomsSwitch.layer.cornerRadius = 16.0
+        
     }
     //MARK: - Done Button Functionality
     @IBAction func doneButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
         
     }
+    //MARK: - Reset Button Functionality
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        
+        distanceSwitch.isOn = false
+        roomsSwitch.isOn = false
+        
+        UserDefaults.standard.set(distanceSwitch.isOn, forKey: "distanceSwitchCase")
+        UserDefaults.standard.set(roomsSwitch.isOn, forKey: "roomsSwitchCase")
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     //MARK: - Distance Button Functionality
     @IBAction func distanceIsOn(_ sender: UISwitch) {
-        
+        UserDefaults.standard.set(sender.isOn, forKey: "distanceSwitchCase")
         if sender.isOn == true{
             self.delegate?.dataReceived(data: self.sourceHotels)
         }else{
@@ -50,6 +68,7 @@ class FiltersViewController: UIViewController {
     }
     //MARK: - Room Availability Button Functionality
     @IBAction func roomsIsOn(_ sender: UISwitch) {
+        UserDefaults.standard.set(sender.isOn, forKey: "roomsSwitchCase")
         
         //sourceHotels[0].suites_availability.components(separatedBy: ":").count
         
