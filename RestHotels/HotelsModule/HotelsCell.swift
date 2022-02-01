abe9cde012409eb1bed577a17a194c561388f4d7
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-02-01 10:03:12 +0300

    Create HotelsCell

---
+++ RestHotels/HotelsModule/HotelsCell.swift
@@ -0,0 +1,20 @@
+//
+//  HotelsCell.swift
+//  RestHotels
+//
+//  Created by tambanco 🥳 on 01.02.2022.
+//  Copyright © 2022 Tambanco. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HotelsCell {
+    //reuseId
+    static let reuseId = "HotelsCell"
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+}
